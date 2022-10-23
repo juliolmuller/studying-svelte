d84c760911b87766498330b747f40bce14f061a2
Julio L. Muller <julio_muller@crgl-thirdparty.com>
2022-10-23 13:21:25 -0300

    chore: add SASS/SCSS support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
-import './app.css'
+import './global.scss'
 import App from './App.svelte'
 
-const app = new App({
-  target: document.getElementById('app')
+export default new App({
+  target: document.getElementById('app'),
 })
-
-export default app
